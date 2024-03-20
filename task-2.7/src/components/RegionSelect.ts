89dccd8eb6b76fdf214f7cb51d296b0d5c2dbd28
foxstudent106668 <dmitry.kozich@gmail.com>
2024-03-21 00:36:04 +0400

    Update menu btn; start working on responsive page layout

--- task-2.7/src/components/RegionSelect.ts
+++ task-2.7/src/components/RegionSelect.ts
@@ -15,13 +15,14 @@ export class RegionSelect {
 
 	public setupListeners(): void {
 		this.toggle.addEventListener('click', this.toggleMenu);
-		this.toggle.addEventListener('blur', this.toggleMenu);
+		this.toggle.addEventListener('blur', this.hideMenu);
 		this.options.forEach((op) => op.addEventListener('mousedown', this.onSelect));
 	}
 
 	private toggleMenu = (): void => {
 		this.isOpen = !this.isOpen;
 		this.menuList.classList.toggle('hidden', !this.isOpen);
+		this.toggle.classList.toggle('open', this.isOpen);
 	};
 
 	private onSelect = (event: any) => {
@@ -29,5 +30,11 @@ export class RegionSelect {
 		event.preventDefault();
 		this.select(event.target.dataset.region);
 		this.toggleMenu();
+	};
+
+	private hideMenu = () => {
+		this.isOpen = false;
+		this.menuList.classList.add('hidden');
+		this.toggle.classList.remove('open');
 	};
 }
